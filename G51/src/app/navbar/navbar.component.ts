5ef69582d5e8d5adda1a604e8bdac4fb07b995bd
Catherine Escobar Trujillo <bettelgeuxe@hotmail.com>
2022-03-04 12:54:27 -0500

    navbar
    navbar y librerias index

--- G51/src/app/navbar/navbar.component.ts
+++ G51/src/app/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+nombregrupo: string  ="Grupo 51";
+
   constructor() { }
 
   ngOnInit(): void {
