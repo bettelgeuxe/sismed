86e73b88e359ab068d5af91d0084c76751bddf24
Catherine Escobar <bettelgeuxe@hotmail.com>
2022-03-09 00:32:46 -0500

    login con alertas
    login con alertas

--- G51/src/app/app.component.ts
+++ G51/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'G51';
+
+  //login
+  user_correcto: string ="admininicial";
+  pass_correcto: string ="admin123456";
+
+  //variables para almacenar datos que insertan en el formulario
+  user:string="";
+  pass:string="";
+
+  //variable para avisos de error
+  correcto: number =-1;
+
+  comparar(){
+    if (this.user===this.user_correcto){
+      this.correcto=1; //si es correcto
+      if (this.pass===this.pass_correcto){
+        this.correcto=1;
+      }else{
+        this.correcto=0;
+      }
+    }else{
+      this.correcto=0;
+    }
+  }
 }
