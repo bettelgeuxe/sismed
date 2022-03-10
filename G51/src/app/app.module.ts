582dcb5db6dc55f026f2e8067ffaf5f0e801d9da
Catherine Escobar <bettelgeuxe@hotmail.com>
2022-03-10 15:54:21 -0500

    creacion componentes
    creacion componentes

--- G51/src/app/app.module.ts
+++ G51/src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import {HttpClientModule} from '@angular/common/http';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { ExtraComponent } from './componentes/extra/extra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    PrincipalComponent,
+    FormularioComponent,
+    ExtraComponent
   ],
   imports: [
     BrowserModule,
